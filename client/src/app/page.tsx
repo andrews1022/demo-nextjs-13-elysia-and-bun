3bf9a21ce7617d1c2845c3ded3fa31f59e63b77e
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-07 01:23:24 -0700

    made simple fetch call on home page to elysia server

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,25 @@
-const HomePage = () => {
+type Message = {
+  message: string;
+};
+
+const getMessage = async (): Promise<Message> => {
+  const res = await fetch("http://localhost:8080/");
+
+  if (!res) {
+    throw new Error("Error fetching message");
+  }
+
+  const data = await res.json();
+  return data;
+};
+
+const HomePage = async () => {
+  const message = await getMessage();
   return (
     <div>
       <h1>HomePage</h1>
+
+      {message ? <p>{message.message}</p> : null}
     </div>
   );
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,10 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(8080);
+const app = new Elysia();
+const port = process.env.PORT || 8080;
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+app.get("/", () => ({ message: "GET from Elysia!" }));
+
+app.listen(port, () => {
+  console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+});
